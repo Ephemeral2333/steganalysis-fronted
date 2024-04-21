f74369563f3b17daaadb2645507104c8010a28b7
Ephemeral2333 <liyh_0703@foxmail.com>
2024-04-21 22:15:36 +0800

    改变一下header的tab

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -107,12 +107,12 @@
         </b-navbar> -->
 
         <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-            <el-menu-item index="1">处理中心</el-menu-item>
+            <el-menu-item index="1">隐写分析</el-menu-item>
             <el-menu-item index="2">
-                我的工作台
+                隐写术
             </el-menu-item>
-            <el-menu-item index="3" disabled>消息中心</el-menu-item>
-            <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+            <el-menu-item index="3" disabled>历史记录</el-menu-item>
+            <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">下载结果</a></el-menu-item>
         </el-menu>
     </header>
 </template>
